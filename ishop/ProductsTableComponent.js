8f9a2d4e89f88577a7fa4db882ee28744a7e1f9e
shtmkr <privet.noobs@mail.ru>
2019-03-18 15:46:39 +0300

    + propsType + getDefaultProps()

--- ishop/ProductsTableComponent.js
+++ ishop/ProductsTableComponent.js
@@ -1,5 +1,4 @@
 const appContainer = document.querySelector('#app');
-
 const products = [
     {name: 'TV', price: '1200 USD', img: 'https://cdn.shopify.com/s/files/1/2422/7737/products/LT-65M877_Smart_TV_-_front_2048x.jpg?v=1534444343', count: 1000},
     {name: 'Radio', price: '12 USD', img: 'https://www.panasonic.com/content/dam/pim/mi/en/RF/RF-562/RF-562DDGC/RF-562DDGC-Product_ImageGlobal-1_mi_en.png', count: 10},
@@ -7,8 +6,25 @@ const products = [
     {name: 'CD-ROM', price: '1 USD', img: 'https://ae01.alicdn.com/kf/HTB1axBaSVXXXXXsXVXXq6xXFXXXG/Full-new-for-ASUS-Black-12X-BD-ROM-16X-DVD-ROM-48X-CD-ROM-SATA-Internal.jpg_640x640.jpg', count: 500}
 ];
 const titles = ["product name", "price", "img", "count" ];
+
 const ProductsTable = React.createClass ({
-    displayName: '',
+    displayName: 'productsTable',
+    propsTypes: {
+        name:   React.PropTypes.string.isRequired,
+        items:  React.PropTypes.arrayOf(React.PropTypes.object),
+        titles: React.PropTypes.array,
+    },
+    getDefaultProps() {
+        return {
+            items: [
+                {name: 'Product', price: '999 USD', img: 'No image', count: 999},
+                {name: 'Product', price: '999 USD', img: 'No image', count: 999},
+                {name: 'Product', price: '999 USD', img: 'No image', count: 999},
+                {name: 'Product', price: '999 USD', img: 'No image', count: 999}],
+            name: 'shop',
+            titles: ['title1','title2','title3','title4'],
+        }
+    },
     render(){
         const
             items = this.props.items,
@@ -18,16 +34,14 @@ const ProductsTable = React.createClass ({
             React.DOM.table({className: 'productsTable'},
                 React.DOM.thead(null,
                     React.DOM.tr(null,
-                        titles.map(function (title) {
-                            return React.DOM.th({key: title}, title);
-                        })
+                        titles.map((title) => React.DOM.th({key: title}, title))
                     )
                 ),
                 React.DOM.tbody(null,
-                    items.map(function (row, index) {
+                    items.map((row, index) => {
                         return (
                             React.DOM.tr({key: index},
-                                Object.keys(row).map(function (col, index) {
+                                Object.keys(row).map((col, index) => {
                                     if (col !== 'img') return React.DOM.td({key: index}, row[col]);
                                     else return (
                                         React.DOM.td({key: index},
@@ -42,8 +56,7 @@ const ProductsTable = React.createClass ({
             )
         )
     }
-}
-);
+});
 
 ReactDOM.render(
     React.createElement(ProductsTable, {name: "ishop", items: products, titles: titles}), appContainer,
