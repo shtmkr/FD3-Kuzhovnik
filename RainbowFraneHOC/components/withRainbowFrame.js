546547225c0a26c6563e54de09785dfb76cb52c9
shtmkr <privet.noobs@mail.ru>
2019-05-11 11:52:34 +0300

    + RainbowFrameHOC

--- RainbowFraneHOC/components/withRainbowFrame.js
+++ RainbowFraneHOC/components/withRainbowFrame.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 function withRainbowFrame(colors) {
-    return function (Component) {
+    return function (Comp) {
         return function (props) {
-            let children = <Component {...props}/>;
+            let hoc = <Comp {...props}/>;
             for (let i = 0; i < colors.length; i++) {
-                children =
+                hoc =
                     <div style={{padding: '5px',textAlign: 'center',border: `solid 4px ${colors[i]}`}}>
-                        {children}
+                        {hoc}
                     </div>;
             }
-            return children
+            return hoc
         }
     }
 }
